e5848bef103188f25bfe24c1990846e764e465c0
Admin <admin@MacBook-Pro-Admin.local>
2020-12-14 23:01:07 +0200

    minor changes

--- homework-09/js/main.js
+++ homework-09/js/main.js
@@ -1,5 +1,6 @@
 const createColor = function() {
-	return '#' + Math.floor(Math.random() * 16777215).toString(16);
+    const maxNumber = 16777215;
+	return '#' + Math.floor(Math.random() * maxNumber).toString(16);
 };
 
 const generateBlocksInterval = function() {
