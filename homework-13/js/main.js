9cc082ed7efbaf79b520e0d827e1d01e985e1348
Admin <admin@MacBook-Pro-Admin.local>
2021-01-29 12:25:39 +0200

    fixed bug with not clickable buttons

--- homework-13/js/main.js
+++ homework-13/js/main.js
@@ -12,22 +12,23 @@ idGenerator.next().value
 
 function* newFontGenerator(start){      
     let res = start;
-    
+
     while(true) {
-        const value = yield;
+        let value = yield res;       
         if(value === 'up') res += 2;    
         if(value === 'down') res -= 2;
-        yield res;
     }
 }
 
 const fontGenerator = newFontGenerator(14)
-
+fontGenerator.next();
 const increaseBtn = document.querySelector('.increase');
 const reduceBtn = document.querySelector('.reduce');
 const text = document.querySelector('.text');
 
-increaseBtn.addEventListener('click', () => {
+text.style.fontSize = '14px';
+
+increaseBtn.addEventListener('click', () => { 
     let font = fontGenerator.next('up').value;
     text.style.fontSize = `${font}px`;
 });
